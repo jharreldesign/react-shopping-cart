c769f70e4c8904360f27ceda99692c99d44c3e89
jharreldesign <harrel.jason@gmail.com>
2024-10-28 16:14:31 -0700

    Added full functionality to the app allowing for removal from state and re add to state when remove button is clicked

---
+++ src/components/InventoryList/InventoryList.jsx
@@ -0,0 +1,37 @@
+// src/components/InventoryList/InventoryList.jsx
+
+// css
+import './InventoryList.css';
+
+const InventoryList = (props) => {
+  return (
+    <div className="inventory-list">
+      <h2>{props.title}</h2>
+      <ul>
+        {props.inventory.length ? (
+          props.inventory.map((item) => (
+            <li key={item._id}>
+              <p>{item.name}</p>
+              <p>Price: {item.price}</p>
+              {props.handleAddItem ? (
+                <button onClick={() => props.handleAddItem(item)}>
+                  Add Item
+                </button>
+              ) : (
+                <button onClick={() => props.handleRemoveItem(item)}>
+                  Remove Item
+                </button>
+              )}
+            </li>
+          ))
+        ) : (
+          <li>
+            <p>Empty</p>
+          </li>
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default InventoryList;
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,20 +1,42 @@
-import { inventoryData } from "../../data/data";
+import { useState } from 'react';
+
+import InventoryList from '../../components/InventoryList/InventoryList';
+
+import { inventoryData } from '../../data/data';
+
+import './Shop.css';
 
 const Shop = () => {
-    console.log('Inventory Data: ', inventoryData)
-    return (
-        <main>
-            <h1>Shop</h1>
-            <ul>
-                {inventoryData.map((item) => (
-                    <li key={item._id}>
-                        <p>Name: {item.name}</p>
-                        <p>Price: {item.price}</p>
-                    </li>
-                ))}
-            </ul>
-        </main>
-    )
+  const [shopInventory, setShopInventory] = useState(inventoryData);
+  const [userInventory, setUserInventory] = useState([]);
+
+  const handleAddItem = (item) => {
+    setUserInventory([...userInventory, item]);
+    setShopInventory(shopInventory.filter((el) => el._id !== item._id));
+  };
+
+  const handleRemoveItem = (item) => {
+    setShopInventory([...shopInventory, item]);
+    setUserInventory(userInventory.filter((el) => el._id !== item._id));
+  };
+
+  return (
+    <main>
+      <h1>Shop</h1>
+      <section className="shop-section">
+        <InventoryList
+          title="Shop Inventory"
+          inventory={shopInventory}
+          handleAddItem={handleAddItem}
+        />
+        <InventoryList
+          title="User Inventory"
+          inventory={userInventory}
+          handleRemoveItem={handleRemoveItem}
+        />
+      </section>
+    </main>
+  );
 };
 
 export default Shop;
