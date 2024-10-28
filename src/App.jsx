198ffd119c2ad5dd16d1dcd602326510e4b1fa6f
jharreldesign <harrel.jason@gmail.com>
2024-10-28 15:44:02 -0700

    Add ability to display from our data we have avaliable

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+
+//components
+import Shop from './components/Shop/Shop';
+
+//CSS
 import './App.css'
 
 function App() {
 
-  return (
-    <>
-    <h1>Hello World!</h1>
-    </>
-  )
+  return <Shop />;
 }
 
 export default App
